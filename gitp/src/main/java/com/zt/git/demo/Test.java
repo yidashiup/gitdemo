4c18bcd9b2acca4ec9506724feb9dc917df0d817
liangguoli <978676890@qq.com>
2021-09-03 16:08:49 +0800

    修改

--- gitp/src/main/java/com/zt/git/demo/Test.java
+++ gitp/src/main/java/com/zt/git/demo/Test.java
@@ -5,5 +5,6 @@ import io.micrometer.core.instrument.util.JsonUtils;
 public class Test {
     public static void main(String[] args) {
         System.out.println("ceshi");
+        System.out.println(1.0+1.0);
     }
 }
